935f76fc861477d6d1f45503fdd14d8262cb12bf
Ben Ferreira <benjaminaf@me.com>
2016-10-18 18:56:08 -0400

    First Commit

---
+++ fizzbuzz.js
@@ -0,0 +1,13 @@
+// Fizz Buzz
+for (var num = 1; num <= 100; num++) {
+  if (num % 15 === 0) {
+    console.log('fizzbuzz');
+  } else if (num % 5 === 0) {
+    console.log('buzz')
+  } else if (num % 3 === 0) {
+    console.log('fizz')
+  }
+}
+
+
+
